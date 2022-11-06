b6bdf1c9025fe4672792980022d1c3790e114f3d
ransomeric <106341563+ransomeric@users.noreply.github.com>
2022-11-06 01:08:29 +0100

    'refactor:' deleted unnecessary comment in button components

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Button extends Component {
     render() {
         const { url, text, id, subtext } = this.props.data
 
-        return <a href={url}  id={id} className="btn"> {text} </a>;
+        return id === "contact" ? <NavLink className="btn" to={url}>{text}</NavLink> : <a href={url}  id={id} className="btn"> {text} </a>
     }
 }
 
